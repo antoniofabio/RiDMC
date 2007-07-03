a092003f5f004c7f220656f45bacf3623ce0aaa8
antonio.fabio <antonio.fabio@gmail.com>
2007-07-03 13:40:19 +0200

    upgraded package version number and dependency on idmclib-0.7.0, upgraded changelog

--- RiDMC/src/idmclib/version.h
+++ RiDMC/src/idmclib/version.h
@@ -2,8 +2,8 @@
 #define __IDMC_VERSION_H__
 
 #define IDMC_VERSION_MAJOR 0
-#define IDMC_VERSION_MINOR 6
-#define IDMC_VERSION_MICRO 1
+#define IDMC_VERSION_MINOR 7
+#define IDMC_VERSION_MICRO 0
 
 int idmc_version_major();
 int idmc_version_minor();
