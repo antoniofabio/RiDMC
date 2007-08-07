9c3c8374c119ea72def7f4862ccc3d36be700529
antoniodinarzo <antonio.fabio@gmail.com>
2007-08-07 10:19:17 +0200

    changed cycles.R indentation

--- RiDMC/R/cycles.R
+++ RiDMC/R/cycles.R
@@ -1,18 +1,18 @@
 cycles_find <- function(idmc_model, par, var, period, eps, max.iter=100) {
-	checkModelParVar(idmc_model, par, var)
-	checkPositiveScalar(eps)
-	checkPositiveScalar(max.iter)
-	period <- as.integer(period)
-	if(period<1)
-		stop('\'period\' should be an integer >=1')
-	ans <- .Call("ridmc_cycles_find", idmc_model$model, as.double(par), as.double(var), 
-		period, as.double(eps), as.integer(max.iter), PACKAGE='RiDMC')
-	##verify that is a real solution:
-	value <- ans$result
-	for(i in seq_len(period))
-		value <- idmc_model$f(par, value)
-	##if not, clean out results:
-	if(max(abs(value-ans$result))>eps)
-		ans <- NULL
-	return(ans)
+  checkModelParVar(idmc_model, par, var)
+  checkPositiveScalar(eps)
+  checkPositiveScalar(max.iter)
+  period <- as.integer(period)
+  if(period<1)
+    stop('\'period\' should be an integer >=1')
+  ans <- .Call("ridmc_cycles_find", idmc_model$model, as.double(par), as.double(var), 
+    period, as.double(eps), as.integer(max.iter), PACKAGE='RiDMC')
+  ##verify that is a real solution:
+  value <- ans$result
+  for(i in seq_len(period))
+    value <- idmc_model$f(par, value)
+  ##if not, clean out results:
+  if(max(abs(value-ans$result))>eps)
+    ans <- NULL
+  return(ans)
 }
