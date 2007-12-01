87df92f69f11c326fd4dd0c99e6d415e2219b6ee
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2007-12-01 13:35:26 +0100

    fixed xyGrob

--- RiDMC/R/grobs.R
+++ RiDMC/R/grobs.R
@@ -135,7 +135,7 @@ mkPlotChildsAndViewports <- function(contents=NULL, main=NULL, xlab=NULL, ylab=N
 }
 
 ##x/y graph
-xyGrob <- function(x, y, type='l', name=NULL, gp=NULL, vp=NULL) {
+xyGrob <- function(x, y, type='l', xlim, ylim, name=NULL, gp=NULL) {
   lgr <- linesGrob(x, y, name=paste(name, 'lines', sep='.'), default.units='native')
   pgr <- pointsGrob(x, y, name=paste(name, 'points', sep='.'), default.units='native')
   if(type=='l')
@@ -144,23 +144,16 @@ xyGrob <- function(x, y, type='l', name=NULL, gp=NULL, vp=NULL) {
     comps <- list(pgr)
   else if (type=='b')
     comps <- list(lgr, pgr)
-  xlim <- range(x, na.rm=TRUE)
-  ylim <- range(y, na.rm=TRUE)
-  if(is.null(vp))
-    vp <- viewport(xscale=xlim, yscale=ylim)
-  gTree(x=x, y=y, type=type, children=do.call(gList, comps),
-    name=name, gp=gp, vp=vp, cl='xyGrob')
+  if(missing(xlim))
+    xlim <- range(x[is.finite(x)])
+  if(missing(ylim))
+    ylim <- range(y[is.finite(y)])
+  ans <- gTree(x=x, y=y, type=type, children=do.call(gList, comps),
+    name=name, gp=gp, 'xyGrob')
+  ans <- contentsGrob(ans, xlim=xlim, ylim=ylim)
 }
 
 tsGrob <- function(x, name=NULL, gp=NULL, vp=NULL) {
   x <- ts(x)
   xyGrob(time(x), x, type='l', name=name, gp=gp, vp=vp)
 }
-
-xyPlotGrob <- function(x, y, type='l', name=NULL, gp=NULL, vp=NULL) {
-  xyg <- xyGrob(x, y, type, name=paste(name,'xyGrob', sep='.'))
-  box <- rectGrob()
-  
-  gTree(x=x, y=y, type=type, children=gList(xyg, box))
-}
-
