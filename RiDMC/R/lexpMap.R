dae65f12b69f93c44e75bf9fedb0671a574dee99
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2007-12-04 10:12:04 +0100

    started adding lexpMap function

---
+++ RiDMC/R/lexpMap.R
@@ -0,0 +1,71 @@
+LyapunovExponentsMap <- function(idmc_model, par, var, time, eps,
+  par.x = 1, par.x.range, par.x.howMany=100, par.y = 2, par.y.range, par.y.howMany=100) {
+  checkModelParVar(idmc_model, par, var)
+  checkPositiveScalar(time)
+  modelType <- getModelType(idmc_model)
+  var <- as.double(var)
+  m <- idmc_model$model
+  if(modelType=='C') {
+    if(missing(eps)) {
+      eps <- getOption('ts.eps')
+      message('using eps = ', eps)
+    }
+    checkPositiveScalar(eps)
+    eps <- as.double(eps)
+    time <- as.double(time)
+    lexpLocal <- function()
+      .Call("ridmc_lexp_ode", m, as.double(par), var, time, eps, PACKAGE='RiDMC')
+  } else {
+    time <- as.integer(time)
+    lexpLocal <- function(par)
+      .Call("ridmc_lexp", m, as.double(par), var, time, PACKAGE='RiDMC')
+  }
+
+  f <- function(p1, p2) {
+    par[par.x] <- p1
+    par[par.y] <- p2
+    ans <- lexpLocal(par)
+    ans[is.finite(ans) & ans>0] <- 1
+    ans[is.finite(ans) & ans<0] <- -1
+    as.integer(ans)
+  }
+
+  p1seq <- seq(par.x.range[1], par.x.range[2], len=par.x.howMany)
+  p2seq <- seq(par.y.range[1], par.y.range[2], len=par.y.howMany)
+  y <- rep(p2seq, rep.int(length(p1seq), length(p2seq)))
+  x <- rep(p1seq, times = ceiling(length(y)/length(p1seq)))
+  y <- as.list(y)
+  x <- as.list(x)
+  mat <- mapply(f, x, y, SIMPLIFY=FALSE)
+  mat <- array(mat, dim=c(length(p1seq), length(p2seq)))
+  ans <- list()
+  ans$model <- idmc_model
+  ans$var <- var
+  ans$par <- par
+  ans$values <- val
+  ans$par.x <- getModelParNames(m)[par.x]
+  ans$par.x.howMany <- par.x.howMany
+  ans$par.y <- getModelParNames(m)[par.y]
+  ans$par.y.howMany <- par.y.howMany
+  ans$mat <- mat
+  class(ans) <- 'idmc_lexp_map'
+  ans
+}
+
+print.idmc_lexp_map <- function(x, ...) {
+  m <- x$model
+  cat('=iDMC Lyapunov exponents map=\n')
+  cat('Model: ', getModelName(m), '\n')
+  cat('Starting point: ')
+    tmp <- getModelVarNames(m)
+    cat(paste(tmp, x$var, sep=' = ', collapse=', '), '\n')
+  cat('Parameter values: ')
+    tmp <- getModelParNames(m)
+    cat(paste(tmp, x$par, sep=' = ', collapse=', '), '\n')
+  cat('Varying x-axis par.: ', x$par.x, '\n')
+  cat('Varying x-axis par. range: [', paste(x$par.x.range, collapse=', '), ']\n')
+  cat('Varying y-axis par.: ', x$par.y, '\n')
+  cat('Varying y-axis par. range: [', paste(x$par.y.range, collapse=', '), ']\n')
+  mle <- range(apply(x$values, 1, max, na.rm=TRUE), na.rm=TRUE)
+  cat('MLE range: [', paste(format(mle, digits=4), collapse=', '), ']\n')
+}
