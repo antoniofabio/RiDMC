f55af13eaa9580e56b981930fb6149df792d532c
Antonio Di Narzo <antonio@antonio-laptop.(none)>
2008-06-10 16:01:02 +0200

    added basin_multi alloc/free methods C wrappers

---
+++ RiDMC/src/r_basin_multi.c
@@ -0,0 +1,64 @@
+/*
+ridmc: iDMC->R interface
+
+Copyright (C) 2008 Marji Lines and Alfredo Medio.
+
+Written by Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or any
+later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+*/
+#include "ridmc.h"
+#include <idmclib/model.h>
+#include <idmclib/attractor.h>
+#include <idmclib/basin_multi.h>
+
+void ridmc_basin_multi_free(SEXP p);
+
+/* Allocate new 'idmc_basin_slow' object
+m: model object (object is cloned)
+parameters: model parameters (vector will be copied)
+xmin, xmax, xres: x axis ranges and resolution
+ymin, ymax, yres: y axis ranges and resolution
+eps: neighborhood treshold to be used for attractor identification
+attractorLimit: limit number of iterations before encountering an attractor
+attractorIterations: limit number of iterations for drawing an attractor
+xvar, yvar: variables to put on x and y axes, respectively
+startValues: vector of starting values. 'xvar' and 'yvar' elmts will be ignored.
+returns: allocated basin object
+*/
+SEXP ridmc_basin_multi_alloc(SEXP model, SEXP parms,
+	SEXP xmin, SEXP xmax, SEXP xres,
+	SEXP ymin, SEXP ymax, SEXP yres,
+	SEXP eps, SEXP attractorLimit, SEXP attractorIterations, SEXP ntries,
+	SEXP xvar, SEXP yvar, SEXP startValues) {
+	SEXP ans;
+	int ians;
+	idmc_basin_multi *basin;
+
+	ians = idmc_basin_multi_alloc(R_ExternalPtrAddr(model), REAL(parms),
+		REAL(xmin)[0], REAL(xmax)[0], INTEGER(xres)[0],
+		REAL(ymin)[0], REAL(ymax)[0], INTEGER(yres)[0],
+		REAL(eps)[0], INTEGER(attractorLimit)[0], INTEGER(attractorIterations)[0],
+		INTEGER(ntries)[0], INTEGER(xvar)[0], INTEGER(yvar)[0], REAL(startValues), &basin);
+	if(ians != IDMC_OK)
+		RIDMC_GENERIC_ERROR(ians);
+	PDEBUG("allocated basin %p\n", basin);
+	PROTECT(ans = R_MakeExternalPtr(basin, R_NilValue, R_NilValue));
+	R_RegisterCFinalizer(ans, ridmc_basin_multi_free);
+	UNPROTECT(1);
+	return ans;
+}
+
+/*deallocates an idmc_basin_multi object*/
+void ridmc_basin_multi_free(SEXP p) {
+	PDEBUG("deallocating basin %p\n", R_ExternalPtrAddr(p));
+	idmc_basin_multi_free( R_ExternalPtrAddr(p) );
+}
