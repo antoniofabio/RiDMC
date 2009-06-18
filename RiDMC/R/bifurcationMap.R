56bd1f1bf21e9a9de44202decf89c404e08445fc
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-06-18 16:40:32 +0200

    added draft bifurcationMap R function

---
+++ RiDMC/R/bifurcationMap.R
@@ -0,0 +1,53 @@
+BifurcationMap <- function(idmc_model,
+                           par, var,
+                           which.var.monitor=1,
+                           which.par.x=1, par.x.range, par.x.howMany=100,
+                           which.par.y=2, par.y.range, par.y.howMany=100,
+                           transient=100, max.period=50,
+                           eps=1e-3) {
+  m <- idmc_model
+  parFull <- numeric(getModelNPar(m))
+  names(parFull) <- getModelParNames(m)
+  parFull[names(par)] <- par
+  par <- parFull
+  checkModelParVar(idmc_model, par, var, txt=deparse(substitute(idmc_model)))
+  par.x.values <- seq(par.x.range[1], par.x.range[2], length=par.x.howMany)
+  par.y.values <- seq(par.y.range[1], par.y.range[2], length=par.y.howMany)
+  values <- .Call('ridmc_bifurcation_map', m$model,
+                  as.double(par), as.double(var),
+                  as.integer(c(which.par.x, which.par.y)),
+                  as.double(par.x.values),
+                  as.double(par.y.values),
+                  as.integer(transient),
+                  as.integer(max.period),
+                  as.double(eps),
+                  PACKAGE='RiDMC')
+  ans <- list()
+  parNames <- getModelParNames(m)
+  names(parNames) <- parNames
+  ans$model <- m
+  values <- matrix(values,
+                   ncol=par.x.howMany,
+                   nrow=par.y.howMany)
+  ans$values <- Raster(xlim=par.x.range, ylim=par.y.range,
+                       xres=par.x.howMany,
+                       yres=par.y.howMany,
+                       data=values)
+  ans$which.par.x <- parNames[which.par.x]
+  ans$which.par.y <- parNames[which.par.y]
+  ans$max.period <- max.period
+  ans$eps <- eps
+  class(ans) <- 'idmc_bifurcation_map'
+  return(ans)
+}
+
+as.matrix.idmc_bifurcation_map <- function(x, ...){
+  return(as.matrix(x$values))
+}
+
+print.idmc_bifurcation_map <- function(x, ...) {
+  cat('= iDMC bifurcation map =\n')
+  cat('Model: ', getModelName(x$model), '\n')
+  cat('varying parameters: (', x$which.par.x, ';', x$which.par.y, ')\n')
+  cat('resolution: ', rasterXres(x$values), 'x', rasterYres(x$values), '\n')
+}
