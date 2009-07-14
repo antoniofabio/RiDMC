0815a36e2afb109daa934e330435193655fe60c7
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-07-14 19:58:44 +0200

    improved color legend grob: now can span multiple columns
    legend editing handling to be fixed in plotGrob

--- RiDMC/R/ribbonGrobs.R
+++ RiDMC/R/ribbonGrobs.R
@@ -38,17 +38,35 @@ widthDetails.ribbonLegend <- function(x)
 ##################
 ##Colors legend ##
 ##################
-colorLegendGrob <- function(colors, labels, x=unit(0,'npc'), y=unit(0, 'npc'), name=NULL, gp=NULL, vp=NULL) {
+colorLegendGrob <- function(colors, labels,
+                            x=unit(0,'npc'), y=unit(0, 'npc'),
+                            numColumns=1,
+                            name=NULL, gp=NULL, vp=NULL) {
   nv <- length(colors)
   if(missing(labels))
     labels <- as.character(seq_along(colors))
-  ys <- unit(1, 'npc') - unit(seq_len(nv) + 1, 'lines')
-  xs0 <- unit(0.5, 'lines')
-  xs1 <- unit(2, 'lines')
-  rg <- rectGrob(x=xs0+x, y=ys-y, width=unit(0.6, 'lines'), height=unit(0.6, 'lines'), just=c('left','bottom'), 
-    gp=gpar(fill=colors), name='rect')
+  if(numColumns > 1) {
+    numRows <- ceiling(nv / numColumns)
+    ys <- unit(1, 'npc') - unit((seq_len(nv)-1) %% numRows + 2, 'lines')
+    xs0 <- (unit(0.5, 'lines') +
+            .colorLegendColumnWidth(labels=labels) *
+            (seq_len(numColumns) - 1))[rep(seq_len(numColumns),
+                                           each=numRows)][seq_len(nv)]
+  } else {
+    ys <- unit(1, 'npc') - unit(seq_len(nv) + 1, 'lines')
+    xs0 <- unit(0.5, 'lines')
+  }
+  xs1 <- xs0 + unit(1.1, 'lines')
+  rg <- rectGrob(x=xs0+x, y=ys-y,
+                 width=unit(0.6, 'lines'),
+                 height=unit(0.6, 'lines'), just=c('left','bottom'), 
+                 gp=gpar(fill=colors), name='rect')
   lg <- textGrob(labels, x=xs1+x, y=ys-y, just=c('left','bottom'), name='text')
-  gTree(colors=colors, labels=labels, x=x, y=y, name=name, gp=gp, vp=vp, children=gList(rg, lg), cl='colorLegendGrob')
+  gTree(colors=colors, labels=labels, x=x, y=y, numColumns=numColumns,
+        name=name, gp=gp, vp=vp, children=gList(rg, lg), cl='colorLegendGrob')
+}
+.colorLegendColumnWidth <- function(obj, labels=obj$labels) {
+  max(stringWidth(labels)) + unit(2.1, 'lines')
 }
 grid.colorLegend <- function(...){
   grid.draw(colorLegendGrob(...))
@@ -56,8 +74,11 @@ grid.colorLegend <- function(...){
 editDetails.colorLegendGrob <- function(x, specs) {
   if(any(c('x','y') %in% names(specs))) {
     x <- colorLegendGrob(x$colors, x$labels, 
-      if(!is.null(specs$x)) specs$x else x$x,
-      if(!is.null(specs$y)) specs$y else x$y)
+                         if(!is.null(specs$x)) specs$x else x$x,
+                         if(!is.null(specs$y)) specs$y else x$y)
+  }
+  if('numColumns' %in% names(specs)) {
+    x <- colorLegendGrob(x$colors, x$labels, x$x, x$y, numColumns=specs$numColumns)
   }
   if('colors' %in% names(specs)) {
     x <- editGrob(x, 'rect', gp=gpar(fill=specs$colors))
@@ -69,8 +90,8 @@ editDetails.colorLegendGrob <- function(x, specs) {
 }
 
 widthDetails.colorLegendGrob <- function(x) {
-  max(stringWidth(x$labels)) + unit(2, 'lines')
+  .colorLegendColumnWidth(x) * x$numColumns + unit(0.5, 'lines')
 }
 heightDetails.colorLegendGrob <- function(x) {
-  unit(length(x$colors), 'lines')
+  unit(ceiling(length(x$labels) / x$numColumns), 'lines')
 }
--- RiDMC/tests/plotGrob.R
+++ RiDMC/tests/plotGrob.R
@@ -2,7 +2,7 @@ library(RiDMC)
 model <- Model(exModelFile('lorenz'))
 trajectory <- Trajectory(model,
 	par=c(10, 28, 2.667), var=c(1.0, 2.0, 1.0),
-	time=200, transient=100, eps=0.05 )
+	time=200, transient=100, eps=0.05)
 trG <- plot(trajectory)
 stopifnot(inherits(trG, 'grob'))
 
@@ -19,3 +19,18 @@ grid.edit(trG$name, xlab="a", ylab="b")
 grid.edit(trG$name, axes=FALSE)
 grid.edit(gPath(trG$name, "xy", "xy.lines"), gp=gpar(lwd=2, col="red"))
 grid.edit(trG$name, legendObj=clg)
+
+##Test color legend:
+grid.newpage()
+clg <- colorLegendGrob(rainbow(19), labels=letters[1:19], numColumns=2)
+pushViewport(viewport(width=unit(4, "lines"), height=unit(4, "lines")))
+grid.draw(clg)
+grid.edit("colorLegend", grep=TRUE, numColumns=1)
+grid.edit("colorLegend", grep=TRUE, numColumns=3)
+grid.edit("colorLegend", grep=TRUE, numColumns=4)
+
+##Test editing color legend in a plotGrob:
+clg <- colorLegendGrob(colors=rainbow(7), labels=letters[1:7])
+pG <- plotGrob(as.grob(trajectory), legendObj=clg)
+grid.draw(pG)
+grid.edit("colorLegend", grep=TRUE, numColumns=3) ##FIXME
