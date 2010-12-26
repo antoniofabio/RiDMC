6a6b65292caf369e37b63e151ec2d31479d9212c
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-12-26 11:25:11 +0100

    tidying multacc model source

--- RiDMC/inst/models/Economic/multacc.lua
+++ RiDMC/inst/models/Economic/multacc.lua
@@ -1,29 +1,38 @@
-name = "multiplier accelerator model"
-description = "Samuelson model with heterogeneous expectations"
+name = "multiplier accelerator"
+description = "Samuelson model with het. expectations"
 type = "D"
 parameters = {"i", "b", "gamma", "mu1", "mu2", "k"}
 variables = {"Y", "Z"}
 
--- gamma stands for gamma^2 in the original model
--- to conform with the original model replace gamma by gamma^2 everywhere below
-
 function f(i, b, gamma, mu1, mu2, k, Y, Z)
-	eq = i/(1-b)
+  eq = i/(1-b)
+  devY = (Y - eq)/eq
+  devZ = (Z - eq)/eq
+  wY = 1 / (1 + gamma * devY^2)
+  wZ = 1 / (1 + gamma * devZ^2)
+  e1Y = Y + mu1 * (Y - eq)
+  e2Y = Y + mu2 * (eq - Y)
+  e1Z = Z + mu1 * (Z - eq)
+  e2Z = Z + mu2 * (eq - Z)
+  eY = wY * e1Y + (1-wY) * e2Y
+  eZ = wZ * e1Z + (1-wZ) * e2Z
+  Y1 = i + b * (1+k) * eY - k * b * eZ
+  Z1 = Y
+  return  Y1, Z1
+end
 
-	devY = (Y - eq)/eq
-	devZ = (Z - eq)/eq
-
-	e1Y = Y + mu1 * (Y - eq)
-	e2Y = Y + mu2 * (eq - Y)
-
-	e1Z = Z + mu1 * (Z - eq)
-	e2Z = Z + mu2 * (eq - Z)
-
-	wY = 1 / (1 + (gamma * devY)^2)
-	wZ = 1 / (1 + (gamma * devZ)^2)
-
-	Y1 = i + b*(1+k) * (wY * e1Y + (1-wY) * e2Y) - k*b*(wZ * e1Z + (1-wZ) * e2Z)
-	Z1 = Y
-
-	return  Y1, Z1
+function Jf(i, b, gamma, mu1, mu2, k, Y, Z)
+  eq = i/(1-b)
+  devY = (Y - eq)/eq
+  devZ = (Z - eq)/eq
+  wY = 1 / (1 + gamma * devY^2)
+  wZ = 1 / (1 + gamma * devZ^2)
+  J11 = b * (1 + k) * wY *
+      (gamma * devY^2 * (-2*mu1 -3*mu2 + 1) +
+      (1 + mu1))
+  J12 = -b * k * wZ *
+      (gamma * devZ^2 * (-2*mu1 -3*mu2 + 1) +
+      (1 + mu1))
+  return J11, J12,
+           1,   0
 end
